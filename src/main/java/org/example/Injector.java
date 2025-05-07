6410c74d3b57c29c105f92b8d377079c10a941d7
«Sofa» <gritsay.sofiya@list.ru>
2025-05-07 18:53:17 +0300

    creating a class Injector and creating dependency classes

--- src/main/java/org/example/Injector.java
+++ src/main/java/org/example/Injector.java
@@ -1,40 +1,45 @@
 package org.example;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.FileInputStream;
 import java.lang.reflect.Field;
 import java.util.Properties;
 
-
 public class Injector {
-    static Properties properties = new Properties();
+    private Properties properties;
 
-    static {
-        try (InputStream inputStream = Injector.class.getResourceAsStream("/dependencies.properties")) {
-            properties.load(inputStream);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public Injector() {
+        properties = new Properties();
+        try {
+            properties.load(new FileInputStream("src/main/resources/dependencies.properties"));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load properties file", e);
         }
     }
 
-    public <T> T inject(T instance) {
-        Field[] fields = instance.getClass().getDeclaredFields();
+    public <T> T inject(T object) {
+        Class<?> clazz = object.getClass();
 
-        for (Field field : fields) {
+        for (Field field : clazz.getDeclaredFields()) {
             if (field.isAnnotationPresent(AutoInjectable.class)) {
-                String dependencyClassName = properties.getProperty(field.getType().getName());
-                if (dependencyClassName != null) {
-                    try {
-                        Class<?> dependencyClass = Class.forName(dependencyClassName);
-                        Object dependencyInstance = dependencyClass.getDeclaredConstructor().newInstance();
-                        field.setAccessible(true);
-                        field.set(instance, dependencyInstance);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                Class<?> fieldType = field.getType();
+
+                String implementationClassName = properties.getProperty(fieldType.getName());
+                if (implementationClassName == null) {
+                    throw new RuntimeException("No implementation specified for " + fieldType.getName());
+                }
+
+                try {
+                    Class<?> implementationClass = Class.forName(implementationClassName);
+                    Object implementationInstance = implementationClass.getDeclaredConstructor().newInstance();
+
+                    field.setAccessible(true);
+                    field.set(object, implementationInstance);
+                } catch (Exception e) {
+                    throw new RuntimeException("Failed to inject dependency for field " + field.getName(), e);
                 }
             }
         }
-        return instance;
+
+        return object;
     }
 }
